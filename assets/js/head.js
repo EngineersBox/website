6a1ed67752b7c7fd31022161309f85677aa5d9d1
EngineersBox <EngineersBox@users.noreply.github.com>
2026-01-25 13:27:51 +0000

    deploy: b2eb3b0e8dc01bbfd1ef49376ac2099f562d5fa0

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,1 +1,18 @@
 document.documentElement.dataset.theme = localStorage.getItem('currentTheme') || 'light';
+
+document.addEventListener("DOMContentLoaded", register_callbacks);
+function register_callbacks() {
+	const slider = document.getElementById("content-width");
+	const slider_label = document.getElementById("content-width-output");
+	slider.addEventListener("change", set_container_width);
+	function set_container_width() {
+		const containers = document.getElementsByClassName("Container");
+		for (const container of containers) {
+			container.style.setProperty("max-width", `${slider.value}%`);
+		}
+	}
+	slider.addEventListener("input", set_width_label);
+	function set_width_label(event) {
+		slider_label.textContent = `${event.target.value}%`;
+	}
+}
